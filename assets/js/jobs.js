be24fb500ca518d00f43740953ba0151bfc8f291
masood-66 <masoodaslamkhan09@gmail.com>
2023-12-07 22:39:48 +0500

    first commit

---
+++ assets/js/jobs.js
@@ -0,0 +1,43 @@
+//$(document).ready(function() {
+//$('.search-box').focus();
+//});
+
+const wrapper = document.querySelector(".wrapper");
+const header = document.querySelector(".header");
+
+
+
+const toggleButton = document.querySelector(".dark-light");
+
+
+const jobCards = document.querySelectorAll(".job-card");
+const logo = document.querySelector(".logo");
+const jobLogos = document.querySelector(".job-logos");
+const jobDetailTitle = document.querySelector(
+ ".job-explain-content .job-card-title"
+);
+const jobBg = document.querySelector(".job-bg");
+
+jobCards.forEach((jobCard) => {
+ jobCard.addEventListener("click", () => {
+  const number = Math.floor(Math.random() * 10);
+  const url = `https://unsplash.it/640/425?image=${number}`;
+  jobBg.src = url;
+
+  const logo = jobCard.querySelector("svg");
+  const bg = logo.style.backgroundColor;
+  console.log(bg);
+  jobBg.style.background = bg;
+  const title = jobCard.querySelector(".job-card-title");
+  jobDetailTitle.textContent = title.textContent;
+  jobLogos.innerHTML = logo.outerHTML;
+  wrapper.classList.add("detail-page");
+  wrapper.scrollTop = 0;
+ });
+});
+
+logo.addEventListener("click", () => {
+ wrapper.classList.remove("detail-page");
+ wrapper.scrollTop = 0;
+   jobBg.style.background = bg;
+});
